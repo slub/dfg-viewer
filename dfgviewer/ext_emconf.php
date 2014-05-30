4cff504e1ca8d667ffac5775e4736a298971aaa6
Sebastian Meyer <sebastian.meyer@slub-dresden.de>
2014-05-30 17:55:53 +0200

    Add dependency information for supported TYPO3 versions.

--- dfgviewer/ext_emconf.php
+++ dfgviewer/ext_emconf.php
@@ -43,9 +43,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-',
-			'typo3' => '4.5.0-',
-			't3jquery' => '2.6.0-',
-			'dlf' => '1.2.0-',
+			'typo3' => '4.5.0-4.7.99',
+			'dlf' => '1.2.1-',
 		),
 		'conflicts' => array(
 		),
