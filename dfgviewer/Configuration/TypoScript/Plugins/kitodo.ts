9eed42e796b8b152a9656614ebc4b01937a111a4
Alexander Bigga <alexander.bigga@slub-dresden.de>
2017-03-21 20:22:37 +0100

    add navigation templates

--- dfgviewer/Configuration/TypoScript/Plugins/kitodo.ts
+++ dfgviewer/Configuration/TypoScript/Plugins/kitodo.ts
@@ -37,6 +37,24 @@ plugin.tx_dlf_metadata {
 	templateFile = {$config.templateFileMeta}
 }
 
+lib.navigation_pagecontrol = USER
+lib.navigation_pagecontrol {
+	includeLibs = typo3conf/ext/dlf/plugins/navigation/class.tx_dlf_navigation.php
+	userFunc = tx_dlf_navigation->main
+	pages = {$config.storagePid}
+	pageStep = 10
+	templateFile = EXT:dfgviewer/Resources/Private/Templates/Plugins/Kitodo/navigation-pagecontrol.tmpl
+}
+
+lib.navigation_viewfunction = USER
+lib.navigation_viewfunction {
+	includeLibs = typo3conf/ext/dlf/plugins/navigation/class.tx_dlf_navigation.php
+	userFunc = tx_dlf_navigation->main
+	pages = {$config.storagePid}
+	pageStep = 10
+	templateFile = EXT:dfgviewer/Resources/Private/Templates/Plugins/Kitodo/navigation-viewfunction.tmpl
+}
+
 plugin.tx_dlf_navigation {
 	pages = {$config.storagePid}
 	pageStep = 5
