725556f1a3e84ae3b10d2b018ee29f9b794a6163
Alexander Bigga <alexander.bigga@slub-dresden.de>
2017-04-29 21:21:47 +0200

    add breadcrumb navigation

---
+++ dfgviewer/Configuration/TypoScript/Navigation/menu.ts
@@ -0,0 +1,87 @@
+# ----------------------------------------
+# Navigation
+# 1. + 2. Level
+# ----------------------------------------
+lib.menu {
+	main = HMENU
+	main {
+		excludeUidList =
+		maxItems = 6
+		entryLevel = 0
+	  1 = TMENU
+	  1 {
+	    expAll = 1
+	    wrap (
+	       <nav class="mainNav">
+	           <div class="navContainer">
+	              <div class="sizeContainer">
+	                 <ul>|</ul>
+	                 <div class="clearfix"></div>
+	              </div>
+	           </div>
+	       </nav>
+	    )
+	    noBlur = 1
+	    accessKey = 1
+	    NO {
+	      wrapItemAndSub = <li>|<div class="clearfix"></div></li>
+
+	      ATagParams =
+	      stdWrap.wrap =
+	      stdWrap = upper
+	      ATagTitle.field = description // title
+	    }
+
+	    #ACT = 1
+	    ACT < .1.NO
+	    ACT {
+	      #wrapItemAndSub = <li class="act">|</li>
+	      stdWrap = upper
+	      ATagParams =
+	      stdWrap.wrap =
+	      #after = <div class="navact"></div>
+	      ATagTitle.field = description // title
+	        }
+
+	    #CURIFSUB < .1.NO
+	    #CURIFSUB.allWrap = <li id="curifsub">|
+
+	    #ACTIFSUB < .1.CURIFSUB
+
+	    }
+	  2 = TMENU
+	  2 {
+	    noBlur = 1
+	    accessKey = 1
+	    wrap = <ul>|</ul><div class="clearfix"></div>
+
+	    NO = 1
+	    NO.ATagTitle.field = description // title
+	    NO.allWrap = <li>|</li>
+	  }
+	}
+	# ----------------------------------------
+	# Breadcrumb Navigation (on Subsites only)
+	# ----------------------------------------
+	breadCrumbNavigation = HMENU
+	breadCrumbNavigation {
+	  special = rootline
+	  special.range = 1|-1
+	  1 = TMENU
+	  1 {
+	    expAll = 1
+	    noBlur = 1
+      accessKey = 1
+	    wrap = <ol class="breadcrumb">|</ol>
+	    NO = 1
+	    NO {
+	      wrapItemAndSub = <li>|</li>
+        ATagTitle.field = title
+	    }
+
+	    CUR = 1
+	    CUR < .NO
+	    #CUR.doNotLinkIt = 1
+	  }
+	}
+}
