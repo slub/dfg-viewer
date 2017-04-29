c6ad83e8f88d9c18b0e1696ca0f4b1d9358fcf47
Alexander Bigga <alexander.bigga@slub-dresden.de>
2017-04-29 21:36:05 +0200

    add language menu

--- dfgviewer/Configuration/TypoScript/Navigation/menu.ts
+++ dfgviewer/Configuration/TypoScript/Navigation/menu.ts
@@ -60,6 +60,26 @@ lib.menu {
 	    NO.allWrap = <li>|</li>
 	  }
 	}
+  secondary = COA
+	secondary {
+    20 = HMENU
+	  20 {
+  	    special = directory
+  	    special.value = {$config.headNavPid}
+        #special.value.current = 1
+  	    1 = TMENU
+  	    1 {
+  	      expAll = 1
+  	      noBlur = 1
+  	      NO = 1
+  	      NO {
+  	        wrapItemAndSub = <li>|</li>
+  	        ATagTitle.field = description // title
+  	      }
+  	    }
+	  }
+	  wrap = <ul class="secondary-nav">|</ul>
+  }
 	# ----------------------------------------
 	# Breadcrumb Navigation (on Subsites only)
 	# ----------------------------------------
