63937609b1360195e3e8201f85a888d9891b9bd5
Alexander Bigga <alexander.bigga@slub-dresden.de>
2017-05-30 12:40:49 +0200

    add workaround for work download of SLUB objects

--- dfgviewer/Classes/ViewHelpers/XpathViewHelper.php
+++ dfgviewer/Classes/ViewHelpers/XpathViewHelper.php
@@ -55,6 +55,14 @@ class XpathViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelpe
 
         $result = $doc->getXpath($xpath);
 
-        return htmlspecialchars(trim((string) $result[0]));
+        if (is_array($result)) {
+          foreach ($result as $row) {
+            $output .= trim($row) . ' ';
+          }
+        } else {
+          $output = trim($result);
+        }
+
+        return htmlspecialchars(trim($output));
     }
 }
