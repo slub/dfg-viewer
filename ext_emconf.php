8c0fe70ac6c1cc0fa34dbd8ede24c80fae8c1e34
Beatrycze Volk <beatrycze.kmiec@slub-dresden.de>
2019-07-15 14:18:11 +0200

    Add autoload to ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -52,4 +52,11 @@ $EM_CONF[$_EXTKEY] = array(
         ),
     ),
     '_md5_values_when_last_written' => '',
+    'autoload' =>
+        array(
+            'psr-4' =>
+                array(
+                    'Slub\\Dfgviewer\\' => 'Classes'
+                )
+        )
 );
