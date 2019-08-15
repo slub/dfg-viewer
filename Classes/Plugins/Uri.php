c1ffa1257e3b815fb53bcf0bd589f2ee44c82d73
Beatrycze Volk <beatrycze.kmiec@slub-dresden.de>
2019-08-15 14:06:07 +0200

    Fix formatting in Uri plugin class

--- Classes/Plugins/Uri.php
+++ Classes/Plugins/Uri.php
@@ -1,28 +1,29 @@
 <?php
+
 namespace Slub\Dfgviewer\Plugins;
 
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2012 Sebastian Meyer <sebastian.meyer@slub-dresden.de>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2012 Sebastian Meyer <sebastian.meyer@slub-dresden.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 use Kitodo\Dlf\Common\AbstractPlugin;
 use TYPO3\CMS\Core\Service\MarkerBasedTemplateService;
@@ -42,148 +43,119 @@ use TYPO3\CMS\Core\Utility\MathUtility;
  * @subpackage  tx_dfgviewer
  * @access  public
  */
-class Uri extends AbstractPlugin {
+class Uri extends AbstractPlugin
+{
+    public $extKey = 'dfgviewer';
 
-  public $extKey = 'dfgviewer';
+    public $scriptRelPath = 'Classes/Plugins/Uri.php';
 
-  public $scriptRelPath = 'Classes/Plugins/Uri.php';
+    /**
+     * The main method of the PlugIn
+     *
+     * @access  public
+     *
+     * @param string $content : The PlugIn content
+     * @param array $conf : The PlugIn configuration
+     *
+     * @return  string    The content that is displayed on the website
+     */
+    public function main($content, $conf)
+    {
+        $this->init($conf);
 
-  /**
-   * The main method of the PlugIn
-   *
-   * @access  public
-   *
-   * @param  string    $content: The PlugIn content
-   * @param  array    $conf: The PlugIn configuration
-   *
-   * @return  string    The content that is displayed on the website
-   */
-  public function main($content, $conf) {
+        // Load current document.
+        $this->loadDocument();
 
-    $this->init($conf);
+        if ($this->doc === NULL) {
+            // Quit without doing anything if required variables are not set.
+            return $content;
+        }
 
-    // Load current document.
-    $this->loadDocument();
-
-    if ($this->doc === NULL) {
-
-      // Quit without doing anything if required variables are not set.
-      return $content;
-
-    } else {
-
-      // Set default values if not set.
-      // page may be integer or string (pyhsical page attribute)
-      if ( (int)$this->piVars['page'] > 0 || empty($this->piVars['page'])) {
-
-        $this->piVars['page'] = MathUtility::forceIntegerInRange((int)$this->piVars['page'], 1, $this->doc->numPages, 1);
-
-      } else {
-
-        $this->piVars['page'] = array_search($this->piVars['page'], $this->doc->physicalStructure);
-
-      }
-
-    }
+        // Set default values if not set.
+        // page may be integer or string (physical page attribute)
+        if ((int)$this->piVars['page'] > 0 || empty($this->piVars['page'])) {
+            $this->piVars['page'] = MathUtility::forceIntegerInRange((int)$this->piVars['page'], 1, $this->doc->numPages, 1);
+        } else {
+            $this->piVars['page'] = array_search($this->piVars['page'], $this->doc->physicalStructure);
+        }
 
         $templateService = GeneralUtility::makeInstance(MarkerBasedTemplateService::class);
 
-    // Load template file.
-    if (!empty($this->conf['templateFile'])) {
-      $this->template = $templateService->getSubpart($GLOBALS['TSFE']->tmpl->getFileName($this->conf['templateFile']), '###TEMPLATE###');
-    } else {
-      $this->template = $templateService->getSubpart($GLOBALS['TSFE']->tmpl->getFileName('EXT:dfgviewer/Resources/Private/Templates/Plugins/Dfgviewer/Uri.tmpl'), '###TEMPLATE###');
-    }
+        // Load template file.
+        if (!empty($this->conf['templateFile'])) {
+            $this->template = $templateService->getSubpart($GLOBALS['TSFE']->tmpl->getFileName($this->conf['templateFile']), '###TEMPLATE###');
+        } else {
+            $this->template = $templateService->getSubpart($GLOBALS['TSFE']->tmpl->getFileName('EXT:dfgviewer/Resources/Private/Templates/Plugins/Dfgviewer/Uri.tmpl'), '###TEMPLATE###');
+        }
 
-    $markerArray = array (
-      '###URIBOOK###' => '',
-      '###URIPAGE###' => ''
-    );
+        $markerArray = array(
+            '###URIBOOK###' => '',
+            '###URIPAGE###' => ''
+        );
 
-    // Get persistent identifier of book.
-    $uriBook = GeneralUtility::trimExplode(' ', $this->doc->physicalStructureInfo[$this->doc->physicalStructure[0]]['contentIds'], TRUE);
+        // Get persistent identifier of book.
+        $uriBook = GeneralUtility::trimExplode(' ', $this->doc->physicalStructureInfo[$this->doc->physicalStructure[0]]['contentIds'], TRUE);
 
-    if (empty($uriBook)) {
+        if (empty($uriBook)) {
+            $uriBook = $this->doc->getLogicalStructure($this->doc->toplevelId);
+            $uriBook = GeneralUtility::trimExplode(' ', $uriBook['contentIds'], TRUE);
+        }
 
-      $uriBook = $this->doc->getLogicalStructure($this->doc->toplevelId);
+        if (!empty($uriBook)) {
 
-      $uriBook = GeneralUtility::trimExplode(' ', $uriBook['contentIds'], TRUE);
+            $uris = array();
 
-    }
+            foreach ($uriBook as $uri) {
+                $piUriBook = $this->pi_getLL('uriBook', '', TRUE);
 
-    if (!empty($uriBook)) {
+                if (strpos($uri, 'http:') === 0 || strpos($uri, 'https:') === 0) {
+                    $uris[] = '<a href="' . htmlspecialchars($uri) . '">' . $piUriBook . '</a>';
+                } elseif (strpos($uri, 'urn:') === 0) {
+                    if (strpos($uri, '/fragment/') === FALSE) {
+                        $uris[] = '<a class="persistence-document" href="http://nbn-resolving.de/' . $uri . '">' . $piUriBook . '</a>';
+                    } else {
+                        $uris[] = '<a class="persistence-document" href="http://nbn-resolving.org/' . $uri . '">' . $piUriBook . '</a>';
+                    }
+                }
 
-      $uris = array ();
+            }
 
-      foreach ($uriBook as $uri) {
+            if (!empty($uris)) {
+                $markerArray['###URIBOOK###'] = implode(', ', $uris);
+            }
+        }
 
-        if (strpos($uri, 'http:') === 0 || strpos($uri, 'https:') === 0) {
+        // Get persistent identifier of page.
+        $uriPage = GeneralUtility::trimExplode(' ', $this->doc->physicalStructureInfo[$this->doc->physicalStructure[$this->piVars['page']]]['contentIds'], TRUE);
 
-          $uris[] = '<a href="'.htmlspecialchars($uri).'">'.$this->pi_getLL('uriBook', '', TRUE).'</a>';
+        if (!empty($uriPage)) {
 
-        } elseif (strpos($uri, 'urn:') === 0) {
+            $uris = array();
 
-          if (strpos($uri, '/fragment/') === FALSE) {
+            foreach ($uriPage as $uri) {
+                $piUriPage = $this->pi_getLL('uriPage', '', TRUE);
 
-            $uris[] = '<a class="persistence-document" href="http://nbn-resolving.de/' . $uri . '">' . $this->pi_getLL('uriBook', '', TRUE) . '</a>';
+                if (strpos($uri, 'http:') === 0 || strpos($uri, 'https:') === 0) {
+                    $uris[] = '<a class="persistence-page" href="' . htmlspecialchars($uri) . '">' . $piUriPage . '</a>';
+                } elseif (strpos($uri, 'urn:') === 0) {
 
-          } else {
+                    if (strpos($uri, '/fragment/') === FALSE) {
+                        $uris[] = '<a class="persistence-page" href="http://nbn-resolving.de/' . $uri . '">' . $piUriPage . '</a>';
+                    } else {
+                        $uris[] = '<a class="persistence-page" href="http://nbn-resolving.org/' . $uri . '">' . $piUriPage . '</a>';
+                    }
 
-            $uris[] = '<a class="persistence-document" href="http://nbn-resolving.org/' . $uri . '">' . $this->pi_getLL('uriBook', '', TRUE) . '</a>';
+                }
 
-          }
+            }
+
+            if (!empty($uris)) {
+                $markerArray['###URIPAGE###'] = implode(', ', $uris);
+            }
 
         }
 
-      }
-
-      if (!empty($uris)) {
-
-        $markerArray['###URIBOOK###'] = implode(', ', $uris);
-
-      }
-
+        return $templateService->substituteMarkerArray($this->template, $markerArray);
     }
-
-    // Get persistent identifier of page.
-    $uriPage = GeneralUtility::trimExplode(' ', $this->doc->physicalStructureInfo[$this->doc->physicalStructure[$this->piVars['page']]]['contentIds'], TRUE);
-
-    if (!empty($uriPage)) {
-
-      $uris = array ();
-
-      foreach ($uriPage as $uri) {
-
-        if (strpos($uri, 'http:') === 0 || strpos($uri, 'https:') === 0) {
-
-          $uris[] = '<a class="persistence-page" href="'.htmlspecialchars($uri).'">'.$this->pi_getLL('uriPage', '', TRUE).'</a>';
-
-        } elseif (strpos($uri, 'urn:') === 0) {
-
-          if (strpos($uri, '/fragment/') === FALSE) {
-
-            $uris[] = '<a class="persistence-page" href="http://nbn-resolving.de/' . $uri . '">' . $this->pi_getLL('uriPage', '', TRUE) . '</a>';
-
-          } else {
-
-            $uris[] = '<a class="persistence-page" href="http://nbn-resolving.org/' . $uri . '">' . $this->pi_getLL('uriPage', '', TRUE) . '</a>';
-
-          }
-
-        }
-
-      }
-
-      if (!empty($uris)) {
-
-        $markerArray['###URIPAGE###'] = implode(', ', $uris);
-
-      }
-
-    }
-
-    return $templateService->substituteMarkerArray($this->template, $markerArray);
-
-  }
 
 }
