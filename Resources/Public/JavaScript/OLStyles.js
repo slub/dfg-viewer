1575a47125dd40bc62e4390f38b8fc59ead36ea9
Christopher Timm <timm@effective-webwork.de>
2024-07-10 17:40:03 +0200

    Codacy fix

--- Resources/Public/JavaScript/OLStyles.js
+++ Resources/Public/JavaScript/OLStyles.js
@@ -7,19 +7,19 @@
 
 
 !*/
-
+/*global ol, dlfViewerOLStyles*/
 /**
  * @return {ol.style.Style}
  */
 dlfViewerOLStyles.hoverStyle = function () {
 
     return new ol.style.Style({
-        'stroke': new ol.style.Stroke({
-            'color': 'rgba(255,255,255,.3)',
-            'width': 2
+        stroke: new ol.style.Stroke({
+            color: "rgba(255,255,255,.3)",
+            width: 2,
         }),
-        'fill': new ol.style.Fill({
-            'color': 'rgba(104, 135, 163,.3)'
+        fill: new ol.style.Fill({
+            color: "rgba(104, 135, 163,.3)",
         })
     });
 
@@ -31,12 +31,12 @@ dlfViewerOLStyles.hoverStyle = function () {
 dlfViewerOLStyles.selectStyle = function () {
 
     return new ol.style.Style({
-        'stroke': new ol.style.Stroke({
-            'color': 'rgba(255,255,255,1)',
-            'width': 2
+        stroke: new ol.style.Stroke({
+            color: "rgba(255,255,255,1)",
+            width: 2
         }),
-        'fill': new ol.style.Fill({
-            'color': 'rgba(255,154,35,.3)'
+        fill: new ol.style.Fill({
+            color: "rgba(255,154,35,.3)",
         })
     });
 
