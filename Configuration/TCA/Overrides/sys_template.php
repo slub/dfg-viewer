b6043dc2a1b967d87f18b8c624748200ec504020
Markus Weigelt <markus.weigelt@slub-dresden.de>
2024-10-21 14:15:43 +0200

    Updates Typo3 12

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die('Access denied.');
 
 // Register static typoscript.
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
