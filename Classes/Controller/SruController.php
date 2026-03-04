35319c51f6470e0b79fd6c726d9a5deac0713eac
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2026-03-04 12:43:48 +0100

    Add missing `languageKey` in SRU plugin

--- Classes/Controller/SruController.php
+++ Classes/Controller/SruController.php
@@ -62,6 +62,7 @@ class SruController extends \Kitodo\Dlf\Controller\AbstractController
         $this->view->assign('sruLink', $sruLink);
         $this->view->assign('currentDocument', $this->document->getLocation());
         $this->view->assign('actionUrl', $actionUrl);
+        $this->view->assign("languageKey", $this->getLanguageService()->lang);
     }
 
     /**
